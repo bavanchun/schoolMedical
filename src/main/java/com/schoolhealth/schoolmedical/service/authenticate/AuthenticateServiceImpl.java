1b280587b33f6d1259b9c2fcd6b0baf17705734c
Ba Van <hoangbavan4478@gmail.com>
2025-06-13 19:22:37 +0700

    Implements user authentication flow
    Adds authentication endpoint using Spring Security.
    Leverages JWT for secure user session management after successful authentication.

--- src/main/java/com/schoolhealth/schoolmedical/service/authenticate/AuthenticateServiceImpl.java
+++ src/main/java/com/schoolhealth/schoolmedical/service/authenticate/AuthenticateServiceImpl.java
@@ -8,6 +8,9 @@ import com.schoolhealth.schoolmedical.model.dto.response.AuthenticationResponse;
 import com.schoolhealth.schoolmedical.repository.UserRepository;
 import com.schoolhealth.schoolmedical.service.user.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -18,7 +21,11 @@ public class AuthenticateServiceImpl implements  AuthenticateService {
 
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
 
     @Override
     public AuthenticationResponse register(RegisterRequest request) {
@@ -43,6 +50,19 @@ public class AuthenticateServiceImpl implements  AuthenticateService {
 
     @Override
     public AuthenticationResponse authenticate(AuthRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getPhoneNumber(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByPhoneNumber(request.getPhoneNumber())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(new HashMap<>(), user);
+
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
     }
 }
