a18b188dbe30976588481abe4c49408d5b855db5
Ba Van <hoangbavan4478@gmail.com>
2025-06-13 15:55:34 +0700

    create UserService update AuthRequest

--- src/main/java/com/schoolhealth/schoolmedical/model/dto/request/AuthRequest.java
+++ src/main/java/com/schoolhealth/schoolmedical/model/dto/request/AuthRequest.java
@@ -1,6 +1,7 @@
 package com.schoolhealth.schoolmedical.model.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import com.schoolhealth.schoolmedical.constant.ValidationConstants;
 import lombok.AllArgsConstructor;
@@ -13,11 +14,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthRequest {
-    @NotBlank(message = "Phone number is required")
+    @NotEmpty(message = "Phone number is required")
     @Pattern(regexp = ValidationConstants.PHONE_NUMBER_REGEX,
             message = "Invalid phone number format. Must be a valid Vietnamese phone number")
     private String phoneNumber;
 
-    @NotBlank(message = "Password is required")
+    @NotEmpty(message = "Password is required")
     private String password;
 }
