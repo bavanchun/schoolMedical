8bdd8883454f38d1d3c54bc85258f3fda0276dfa
Ba Van <hoangbavan4478@gmail.com>
2025-06-13 18:28:38 +0700

    Add AuthenticationManager bean configuration
    - Configure AuthenticationManager bean in ApplicationConfig
    - Enable Spring Security to authenticate user requests

--- src/main/java/com/schoolhealth/schoolmedical/config/ApplicationConfig.java
+++ src/main/java/com/schoolhealth/schoolmedical/config/ApplicationConfig.java
@@ -4,8 +4,10 @@ import com.schoolhealth.schoolmedical.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -33,6 +35,11 @@ public class ApplicationConfig {
         authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(passwordEndcoder());
         return  authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
