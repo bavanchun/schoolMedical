ae573b2be0d4fa70f2c767eb11e24bc50e8e2914
Ba Van <hoangbavan4478@gmail.com>
2025-06-13 17:12:38 +0700

    Implements user authentication with JWT
    Enables user authentication by integrating Spring Security.
    It upgrades project JDK version to 21 and add `spring-boot-starter-security` dependency.
    The changes implement JWT-based authentication, enhances the User entity to conform to Spring Security's UserDetails interface,
    and introduces a JwtService interface to handle JWT-related operations.
    Also, it modify UserRepository to use Optional to handle null cases properly.

--- src/main/java/com/schoolhealth/schoolmedical/repository/UserRepository.java
+++ src/main/java/com/schoolhealth/schoolmedical/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.schoolhealth.schoolmedical.repository;
 import com.schoolhealth.schoolmedical.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
-    User findByPhoneNumber(int phoneNumber);
+
+    Optional<User> findByPhoneNumber(String phoneNumber);
 }
---
+++ src/main/java/com/schoolhealth/schoolmedical/service/user/JwtService.java
@@ -0,0 +1,6 @@
+package com.schoolhealth.schoolmedical.service.user;
+
+public interface JwtService {
+    String extractUserName(String token);
+
+}
