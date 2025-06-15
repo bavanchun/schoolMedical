8b9bd9b4c9f9cf82569b98eebcd25fbb4965852c
Tri <quachtri865@gmail.com>
2025-06-15 16:07:31 +0700

    Refactor DiseaseService to return Optional<List<Disease>>, update HealCheckCampaignImpl to handle no diseases found, and add PupilMapper implementation

--- src/main/java/com/schoolhealth/schoolmedical/service/DiseaseService.java
+++ src/main/java/com/schoolhealth/schoolmedical/service/DiseaseService.java
@@ -3,7 +3,8 @@ package com.schoolhealth.schoolmedical.service;
 import com.schoolhealth.schoolmedical.entity.Disease;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DiseaseService {
-    List<Disease> getAllDiseases();
+    Optional<List<Disease>> getAllDiseases();
 }
