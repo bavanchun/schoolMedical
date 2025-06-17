7ddb49568c2bc98c69aa0fdb022be793cb53b18b
Ba Van <hoangbavan4478@gmail.com>
2025-06-17 17:24:28 +0700

    Adds DiseaseMapper interface
    Introduces the DiseaseMapper interface for mapping between
    Disease entities and DTOs.
    
    This mapper simplifies data transfer between the application
    and the database, improving code maintainability.

---
+++ src/main/java/com/schoolhealth/schoolmedical/model/mapper/DiseaseMapper.java
@@ -0,0 +1,21 @@
+package com.schoolhealth.schoolmedical.model.mapper;
+
+import com.schoolhealth.schoolmedical.entity.Disease;
+import com.schoolhealth.schoolmedical.model.dto.request.DiseaseRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface DiseaseMapper {
+
+    @Mappings({
+            @Mapping(target = "diseaseId", ignore = true),
+            @Mapping(target = "isActive", constant = "true"),
+            @Mapping(target = "campaigns", ignore = true),
+            @Mapping(target = "diseaseVaccines", ignore = true)
+    })
+    Disease toEntity(DiseaseRequest request);
+
+    DiseaseRequest toDto(Disease disease);
+}
