26464e010c006e4a84731d091088d0b45edd4fb6
Ba Van <hoangbavan4478@gmail.com>
2025-06-17 19:06:39 +0700

    Implements DiseaseVaccine management
    Adds API endpoints for managing the relationship between diseases and vaccines.
    This includes creating, deleting, and retrieving disease-vaccine associations.
    The delete operation now deactivates the association instead of physically deleting it.
    Only active disease-vaccine associations are retrieved.

--- src/main/java/com/schoolhealth/schoolmedical/repository/DiseaseVaccineRepository.java
+++ src/main/java/com/schoolhealth/schoolmedical/repository/DiseaseVaccineRepository.java
@@ -4,5 +4,9 @@ import com.schoolhealth.schoolmedical.entity.DiseaseVaccine;
 import com.schoolhealth.schoolmedical.entity.DiseaseVaccineId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DiseaseVaccineRepository extends JpaRepository<DiseaseVaccine, DiseaseVaccineId> {
+    // Phương thức để lấy tất cả DiseaseVaccine có isActive = true
+    List<DiseaseVaccine> findByIsActiveTrue();
 }
---
+++ src/main/java/com/schoolhealth/schoolmedical/service/diseaseVaccine/DiseaseVaccineServiceImpl.java
@@ -0,0 +1,43 @@
+package com.schoolhealth.schoolmedical.service.diseaseVaccine;
+
+import com.schoolhealth.schoolmedical.entity.DiseaseVaccine;
+import com.schoolhealth.schoolmedical.entity.DiseaseVaccineId;
+import com.schoolhealth.schoolmedical.model.dto.request.DiseaseVaccineRequest;
+import com.schoolhealth.schoolmedical.model.dto.response.DiseaseVaccineResponse;
+import com.schoolhealth.schoolmedical.model.mapper.DiseaseVaccineMapper;
+import com.schoolhealth.schoolmedical.repository.DiseaseVaccineRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class DiseaseVaccineServiceImpl implements DiseaseVaccineService{
+
+    private final DiseaseVaccineRepository repository;
+    private final DiseaseVaccineMapper mapper;
+
+    @Override
+    public DiseaseVaccineResponse create(DiseaseVaccineRequest request) {
+        DiseaseVaccine entity = mapper.toEntity(request);
+        return mapper.toDto(repository.save(entity));
+    }
+
+    @Override
+    public void delete(int diseaseId, int vaccineId) {
+        DiseaseVaccine entity = repository.findById(new DiseaseVaccineId(diseaseId, vaccineId))
+                .orElseThrow(() -> new EntityNotFoundException("Mapping not found"));
+        entity.setActive(false);
+        repository.save(entity);
+    }
+
+    @Override
+    public List<DiseaseVaccineResponse> getAll() {
+        return repository.findByIsActiveTrue().stream()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
+    }
+}
