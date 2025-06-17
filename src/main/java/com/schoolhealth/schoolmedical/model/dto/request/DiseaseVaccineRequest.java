88e75f7a1ec4f874a537a82cd1e2ce14b34b21a0
Ba Van <hoangbavan4478@gmail.com>
2025-06-17 18:55:50 +0700

    Adds DiseaseVaccineRequest DTO
    Defines a request DTO for associating diseases and vaccines.
    
    This DTO encapsulates the IDs of both a disease and a vaccine,
    facilitating data transfer when creating or updating disease-vaccine
    relationships.

---
+++ src/main/java/com/schoolhealth/schoolmedical/model/dto/request/DiseaseVaccineRequest.java
@@ -0,0 +1,17 @@
+package com.schoolhealth.schoolmedical.model.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DiseaseVaccineRequest {
+    private int diseaseId;
+    private int vaccineId;
+}
