ac74bb29dae3d0791ea7b4fa82251cad1ff639c0
Ba Van <hoangbavan4478@gmail.com>
2025-06-19 08:50:40 +0700

    Add pagination and isActive filter to getAllVaccines endpoint and service methods

--- src/main/java/com/schoolhealth/schoolmedical/controller/VaccineController.java
+++ src/main/java/com/schoolhealth/schoolmedical/controller/VaccineController.java
@@ -3,7 +3,9 @@ package com.schoolhealth.schoolmedical.controller;
 import com.schoolhealth.schoolmedical.model.dto.request.VaccineRequest;
 import com.schoolhealth.schoolmedical.model.dto.response.VaccineResponse;
 import com.schoolhealth.schoolmedical.service.vaccine.VaccineService;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -43,8 +46,21 @@ public class VaccineController {
         return ResponseEntity.ok(vaccineService.getVaccineById(id));
     }
 
+//    @GetMapping
+//    public ResponseEntity<List<VaccineResponse>> getAll() {
+//        return ResponseEntity.ok(vaccineService.getAllVaccines());
+//    }
     @GetMapping
-    public ResponseEntity<List<VaccineResponse>> getAll() {
-        return ResponseEntity.ok(vaccineService.getAllVaccines());
+    public ResponseEntity<Page<VaccineResponse>> getAll(
+            @Parameter(description = "Page number (starting from 1)", example = "1")
+            @RequestParam(defaultValue = "1") int page,
+
+            @Parameter(description = "Page size (number of records per page)", example = "20")
+            @RequestParam(defaultValue = "20") int size,
+
+            @Parameter(description = "Filter by active status", example = "true")
+            @RequestParam(defaultValue = "true") boolean isActive) {
+        Page<VaccineResponse> vaccine = vaccineService.getAllVaccines(page, size, isActive);
+        return ResponseEntity.ok(vaccine);
     }
 }
