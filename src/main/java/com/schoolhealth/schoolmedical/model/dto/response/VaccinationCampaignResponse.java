0d500e47dc160523fd4e791338b4034e8f12f94c
Ba Van <hoangbavan4478@gmail.com>
2025-06-19 19:38:52 +0700

    Add VaccinationCampaignResponse DTO for campaign details and response structure

---
+++ src/main/java/com/schoolhealth/schoolmedical/model/dto/response/VaccinationCampaignResponse.java
@@ -0,0 +1,34 @@
+package com.schoolhealth.schoolmedical.model.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.schoolhealth.schoolmedical.entity.enums.VaccinationCampaignStatus;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VaccinationCampaignResponse {
+    private int campaignId;
+    private String titleCampaign;
+    private int vaccineId;
+    private int diseaseId;
+    private int doseNumber;
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate startDate;
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate endDate;
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate formDeadline;
+
+    private String notes;
+    private VaccinationCampaignStatus status;
+    private boolean isActive;
+}
