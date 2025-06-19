5b0d42d61b0d5fdfd24d58402031e7c6cd484fc9
Ba Van <hoangbavan4478@gmail.com>
2025-06-19 15:37:32 +0700

    Add validation annotations to DiseaseVaccineRequest fields for improved request validation

--- src/main/java/com/schoolhealth/schoolmedical/model/dto/request/DiseaseVaccineRequest.java
+++ src/main/java/com/schoolhealth/schoolmedical/model/dto/request/DiseaseVaccineRequest.java
@@ -1,5 +1,8 @@
 package com.schoolhealth.schoolmedical.model.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +15,12 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder
 public class DiseaseVaccineRequest {
+
+    @NotNull(message = "Disease ID cannot be null")
+    @Positive(message = "Disease ID must be a positive integer")
     private int diseaseId;
+
+    @NotNull(message = "Vaccine ID cannot be null")
+    @Positive(message = "Vaccine ID must be a positive integer")
     private int vaccineId;
 }
