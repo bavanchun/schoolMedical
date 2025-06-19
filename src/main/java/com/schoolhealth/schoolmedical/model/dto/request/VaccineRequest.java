2761ed21e850aa08cfb98208bc9e22e43bb65238
Ba Van <hoangbavan4478@gmail.com>
2025-06-19 08:58:48 +0700

    Add validation annotations to VaccineRequest fields for improved request validation

--- src/main/java/com/schoolhealth/schoolmedical/model/dto/request/VaccineRequest.java
+++ src/main/java/com/schoolhealth/schoolmedical/model/dto/request/VaccineRequest.java
@@ -1,5 +1,8 @@
 package com.schoolhealth.schoolmedical.model.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,9 +15,21 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder
 public class VaccineRequest {
+    @NotBlank(message = "Name vaccine should not be blank")
+    @Size(max = 255, message = "Name vaccine should not exceed 255 characters")
     private String name;
+
+    @NotBlank(message = "Manufacturer should not be blank")
+    @Size(max = 255, message = "Manufacturer should not exceed 255 characters")
     private String manufacturer;
+
+    @NotBlank(message = "Recommended age should not be blank")
+    @Size(max = 100, message = "Recommended age should not exceed 100 characters")
     private String recommendedAge;
+
+    @NotBlank(message = "Description should not be blank")
     private String description;
+
+    @Min(value = 1, message = "Dose number should be at least 1")
     private int doseNumber;
 }
