e236c55a1bb626938364030d3a28ab255cc571be
Tri <quachtri865@gmail.com>
2025-06-24 21:52:30 +0700

    Implement notification sending for health check campaigns; refactor FCM service to handle multiple pupils and update user retrieval logic

--- src/main/java/com/schoolhealth/schoolmedical/service/Notification/FCMImpl.java
+++ src/main/java/com/schoolhealth/schoolmedical/service/Notification/FCMImpl.java
@@ -1,10 +1,12 @@
 package com.schoolhealth.schoolmedical.service.Notification;
 
 import com.google.firebase.messaging.*;
+import com.schoolhealth.schoolmedical.entity.Pupil;
 import com.schoolhealth.schoolmedical.entity.UserNotification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -15,23 +17,40 @@ public class FCMImpl implements FCMService{
     private FirebaseMessaging firebaseMessaging;
 
     @Override
-    public String sendNotification(UserNotification notification, String token) {
-        Message message = Message.builder()
-                .setToken(token)
-                .putData("title", notification.getMessage())
-                .putAllData(Map.of(
-                        "campaignId", String.valueOf(notification.getSourceId()),
-                        "campaignType", notification.getTypeNotification().toString(),
-                        "parentId", String.valueOf(notification.getUser().getUserId())
-                ))
-                .build();
+    public void sendNotification(Map<String,List<Pupil>> pupilsMap, Long sourceId, String type, String title) {
+        if (pupilsMap == null || pupilsMap.isEmpty()) {
+            System.out.println("Không có học sinh nào để gửi thông báo");
+        }
+        List<Message> messages = new ArrayList<>();
+        for(Map.Entry<String, List<Pupil>> entry : pupilsMap.entrySet()) {
+            String token = entry.getKey();
+            List<Pupil> pupils = entry.getValue();
+
+            if (token == null || token.isEmpty()) {
+                System.out.println("Token không hợp lệ");
+                continue;
+            }
+
+            for (Pupil pupil : pupils) {
+                Message message = Message.builder()
+                        .setToken(token)
+                        .putData("title", title)
+                        .putAllData(Map.of(
+                                "campaignId", String.valueOf(sourceId),
+                                "campaignType", type,
+                                "pupilId", String.valueOf(pupil.getPupilId()),
+                                "pupilName", pupil.getFirstName(),
+                                "pupilLastName", pupil.getLastName()
+                        ))
+                        .build();
+                messages.add(message);
+            }
+        }
         try {
-            String response = firebaseMessaging.send(message);
+            BatchResponse response = firebaseMessaging.sendEach(messages);
             System.out.println("Successfully sent message: " + response);
-            return response;
         } catch (FirebaseMessagingException e) {
             System.err.println("Error sending message: " + e.getMessage());
-            return "Error sending message: " + e.getMessage();
         }
     }
 
