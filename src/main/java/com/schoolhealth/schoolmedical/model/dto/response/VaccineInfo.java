5dabd1c850da4f936043e073bc9a57a4835b0454
Ba Van <hoangbavan4478@gmail.com>
2025-06-24 22:10:09 +0700

    Add DTOs for disease and vaccine information

---
+++ src/main/java/com/schoolhealth/schoolmedical/model/dto/response/VaccineInfo.java
@@ -0,0 +1,15 @@
+package com.schoolhealth.schoolmedical.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VaccineInfo {
+    private Long vaccineId;
+    private String name;
+}
