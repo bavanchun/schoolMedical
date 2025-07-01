44413060844f34701064bc6f0cc1d982fbda0be0
Tri <quachtri865@gmail.com>
2025-07-01 20:27:04 +0700

    Enhance security configuration and refactor SendMedication class: enable method security and rename class for clarity

--- src/main/java/com/schoolhealth/schoolmedical/controller/SendMedicationController.java
+++ src/main/java/com/schoolhealth/schoolmedical/controller/SendMedicationController.java
@@ -1,19 +1,19 @@
 package com.schoolhealth.schoolmedical.controller;
 
-import com.schoolhealth.schoolmedical.entity.enums.StatusSendMedication;
 import com.schoolhealth.schoolmedical.model.dto.request.SendMedicationReq;
 import com.schoolhealth.schoolmedical.model.dto.request.UpdateStatusSendMedicationReq;
 import com.schoolhealth.schoolmedical.model.dto.response.SendMedicationRes;
 import com.schoolhealth.schoolmedical.service.sendMedication.SendMedicalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/send-medication")
-public class SendMedication {
+public class SendMedicationController {
     @Autowired
     private SendMedicalService sendMedicalService;
 
@@ -28,6 +28,7 @@ public class SendMedication {
         return ResponseEntity.ok(sendMedicationRes);
     }
     @PatchMapping("/{sendMedicationId}")
+    @PreAuthorize("hasRole('SCHOOL_NURSE')")
     public ResponseEntity<?> updateStatusSendMedication(@PathVariable Long sendMedicationId, @RequestBody UpdateStatusSendMedicationReq statusSendMedication) {
         // Logic to update send medication will go here
         sendMedicalService.updateStatus(sendMedicationId,statusSendMedication.getStatusSendMedication());
