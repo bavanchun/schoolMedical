054501351833edc59be629aa18e4397671901957
Tri <quachtri865@gmail.com>
2025-07-01 14:44:02 +0700

    Refactor SendMedication functionality: rename package, add service interface, implement status update and retrieval methods

--- src/main/java/com/schoolhealth/schoolmedical/entity/enums/StatusSendMedication.java
+++ src/main/java/com/schoolhealth/schoolmedical/entity/enums/StatusSendMedication.java
@@ -3,6 +3,8 @@ package com.schoolhealth.schoolmedical.entity.enums;
 public enum StatusSendMedication {
     PENDING("Đang chờ"),
     APPROVED("Đã duyệt"),
+    IN_PROGRESS("Đang thực hiện"),
+    COMPLETED("Đã hoàn thành"),
     REJECTED("Đã từ chối");
 
     private final String description;
--- src/main/java/com/schoolhealth/schoolmedical/service/sendMedical/SendMedicalService.java
+++
@@ -1,8 +0,0 @@
-package com.schoolhealth.schoolmedical.service.sendMedical;
-
-import com.schoolhealth.schoolmedical.model.dto.request.SendMedicationReq;
-import com.schoolhealth.schoolmedical.model.dto.response.SendMedicationRes;
-
-public interface SendMedicalService {
-    SendMedicationRes createSendMedication(SendMedicationReq sendMedicationReq, String userId);
-}
